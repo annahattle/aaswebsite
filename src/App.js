9f0f8c44db4d7a8526fd6f0cd48e31a8897ecc73
Anna Hattle <annahattle@annas-mbp-5.wireless-1x.unc.edu>
2019-02-16 22:35:23 -0500

    white bg, react default erased

--- src/App.js
+++ src/App.js
@@ -13,15 +13,15 @@ class App extends Component {
           <p>
           The third recorded iteration of AAPI (Asian American Pacific Islander) Working Group, we are a student-led advocacy group established in 2016.  We advocate for the needs of Asian American and Pacific Islander students at UNC Chapel Hill and are focused on the following goals:
           GOAL #1: SPACE: An Asian American student center that houses AAPI student organizations and their offices, provides social and academic mentorship in the form of libraries, scholarship information, etc and provides a collaborative space for student organizations to create programming and resources catered to UNC’s Asian American students.
-            Edit <code>src/App.js</code> and save to reload.
+
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.facebook.com/unc.aapiwg/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn more
           </a>
         </header>
       </div>
